19862e59787648be125744025a476eeaf4e1d662
sudip ghimire <sudipghimire533@gmail.com>
2020-08-25 23:29:29 +0545

    Basic  Structuring and styling of project

---
+++ home/home.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Help me for Homework</title>
+
+    <link href='../global/global.css' type="text/css" rel="stylesheet" />
+    <link href='./home.css' type="text/css" rel="stylesheet" />
+    <link href='../thread/question_entity.css' type='text/css' rel='stylesheet' />
+    <link rel='stylesheet' type='text/css' href='../global/fs_css/all.css' />
+</head>
+
+<body>
+    <div id='Main'>
+        <div class='QuestionFeed'>
+            <div class='Question'>
+                <div class='questionTitle'>
+                    <i class='qn_status fab fa-gripfire' title='Trending'></i>
+                    <a href='#'>How to force c++ Compiler to perform stack unwinding after the uncaught exception</a>
+                    <span class='quickAction'>
+                        <i class='fas fa-bookmark'></i>
+                        <i class='fas fa-star'></i>
+                        <a href='#' class='fas fa-reply'></a>
+                    </span>
+                </div>
+                <div class='description'>
+                    <span>
+                        I am working with a sample program under Linux specifically Deban buster with the default gcc
+                        compiler of version 10. case it is very important to do so . What is
+                        you idon for this...
+                    </span>
+                </div>
+                <div class='questionInfo'>
+                    <div class='tagContainer'>
+                        <a href='#' class='tag'>C++</a herf='#'>
+                        <a href='#' class='tag'>Linux</a herf='#'>
+                        <a href='#' class='tag'>Technical</a herf='#'>
+                        <a href='#' class='tag'>Compiler</a herf='#'>
+                        <a href='#' class='tag'>University</a herf='#'>
+                    </div>
+                    <div class='asking_user'>
+                        <span>Asked By</span>
+                        <a href='#' class='asker_name hv_border'>Sudip Ghimire</a>
+                        <span> on </span>
+                        <div class='asked_date'>2020-07-34</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+
+
+
+<!--START Temporary Code-->
+<script>
+    let parent = document.querySelector('.QuestionFeed');
+    for (let i = 0; i < 15; i++) {
+        parent.appendChild(document.getElementsByClassName('Question')[0].cloneNode(true));
+    }
+</script>
+<!--END Temporray Code-->
---
+++ login/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Help me for Homework</title>
+
+    <link href='../global/global.css' type="text/css" rel="stylesheet" />
+    <link href='./login.css' type="text/css" rel="stylesheet" />
+</head>
+
+<body>
+    <div id='Login'>
+        <div class='loginConatiner Container'>
+            <div class='left'>
+                <div class='imgContainer'>
+                    <img src='./images/feature.jpg' alt='Featured Image' title='Sign in' />
+                </div>
+            </div>
+            <div class='right'>
+                <div class='facebook_login_btn login_btn'>Login With Facebook
+                </div>
+                <div class='google_login_btn login_btn'>Login With Google
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
