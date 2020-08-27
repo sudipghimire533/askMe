ed321111480ef3c304718e51e6d38798c60bf341
sudip ghimire <sudipghimire533@gmail.com>
2020-08-27 14:54:43 +0545

    Update feed sqlto use left join instead

--- server/get_feed.php
+++ server/get_feed.php
@@ -20,6 +20,7 @@ class Getfeed
     }
     public function Recent()
     {
+        /*
         $res = $this->conn->query("SELECT DISTINCT
                     qn.Title As title,
                     qn.URLTitle AS url,
@@ -28,8 +29,21 @@ class Getfeed
                     ORDER BY qn.LastActive DESC
                     LIMIT 10;
                 ");
+        */
+        $res = $this->conn->query("SELECT DISTINCT
+                    qn.Title AS title,
+                    qn.URLTitle AS url,
+                    SUBSTRING(qn.Description, 1, 270) AS info,
+                    tg.Name As tag
+                    FROM
+                    Question qn LEFT JOIN
+                    QuestionTag qt ON qt.Question=qn.Id LEFT JOIN
+                    Tags tg ON qt.Tag=tg.Id
+                    LIMIT 10
+                    ;");
 
         $response = json_encode($res->fetch_all(MYSQLI_ASSOC));
+
         echo $response;
     }
 };
